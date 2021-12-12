935286dabf9b1bccb0def1ac86195d48523ee6b6
Francisco-Costa98 <franciscomffcosta@hotmail.com>
2021-12-12 19:29:17 +0000

    added rendering html for posts tags and comments

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Post, Comment, Tags
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts =  Post.objects.all()
+    comments = Comment.objects.all()
+    tags = Tags.objects.all()
+    context = {'posts':posts, 'tags':tags, 'comments': comments}   
+    print(context)
+
+    return render(request, 'blog/post_list.html', context)
