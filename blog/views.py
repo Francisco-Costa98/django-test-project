1c06f17d911f7033f25078a01cbacb9cb6a0f3c0
Francisco-Costa98 <franciscomffcosta@hotmail.com>
2022-01-02 22:48:08 +0000

    latest changes

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,26 @@
+from datetime import datetime
 from django.shortcuts import render
 from .models import Post, Comment, Tags
 from django.utils import timezone
+from django.db.models import Q
 
 # Create your views here.
 def post_list(request):
     posts =  Post.objects.all()
-    comments = Comment.objects.all()
-    tags = Tags.objects.all()
-    context = {'posts':posts, 'tags':tags, 'comments': comments}   
-    print(context)
+
+    post_by_month_arr = []
+
+    for month in range(1,13):
+        post_by_month_arr.append({'posts':Post.objects.filter(published_date__month=month), 'month':month})
+    
+    print('post by month array', post_by_month_arr)
+
+
+    context = {'posts':posts, 'postsByMonth':post_by_month_arr}   
 
     return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    post =Post.objects.get(pk=pk)
+    context = { 'post' : post }
+    return render(request, 'blog/post_detail.html', context)
