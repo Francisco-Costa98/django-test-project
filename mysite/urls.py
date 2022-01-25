dbc89f8e9759987f3c862d8b8de07e71ecf441fb
Francisco-Costa98 <franciscomffcosta@hotmail.com>
2022-01-25 23:41:02 +0000

    added permissions and users to models - migrations not working

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('snippets/', include('snippets.urls')),
-
+    path('api-auth/', include('rest_framework.urls')),
 ]
